6afc3692dbcd0bc943a8fca04a9121f4d2a77079
Shimao890 <86386251+Shimao890@users.noreply.github.com>
2022-05-12 21:33:58 -0700

    add lab11

---
+++ lab11/js/lab.js
@@ -0,0 +1,19 @@
+/*
+ * author: Shimao Zhou Cole Keeton
+ * created: May 2nd
+ * license:
+ *
+ */
+ // Libraries & jQuery
+
+$("#butt1").click(function(){
+  $("#Challenges").toggleClass("first");
+})
+
+$("#butt2").click(function(){
+  $("#Problems").toggleClass("second");
+})
+
+$("#butt3").click(function(){
+  $("#Results").toggleClass("third");
+})
